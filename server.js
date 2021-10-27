8fa94ce1fd5e1209511b0e1d7479e144ec847821
mohamadsaad1 <mohamad_utd@outlook.com>
2021-10-27 00:24:28 -0500

    finish

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import * as heroDb from "./data/hero-db.js"
+console.log(heroDb.find)
+// create express application
+
+const app = express()
+
+
+// configure the app (app.set)
+
+app.set("view engine", "ejs")
+
+// mount middleware (app.use)
+
+
+
+// mount routes
+
+// localhost: 3000/
+app.get('/', function(req, res) {
+  res.send('<h1>Dota 2 Heroes</h1>')
+})
+
+
+app.get('/heroes', function(req, res) {
+  heroDb.find({}, function(error, heroes) {
+    res.render('heroes/index', {
+      heroes: heroes,
+      error: error
+    })
+  })
+})
+// tell our app to llisten to port 3000
+
+app.listen(3000, function(){
+})
